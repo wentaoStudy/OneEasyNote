7338c923a66fdb1206fd5f58b9751e51edfccc5a
wentao <2335844083@qq.com>
2019-06-03 10:02:43 +0800

    add note bean

--- app/src/main/java/com/wentaostudy/oneeasynote/MainActivity.java
+++ app/src/main/java/com/wentaostudy/oneeasynote/MainActivity.java
@@ -3,7 +3,20 @@ package com.wentaostudy.oneeasynote;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
